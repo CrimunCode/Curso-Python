2f8fcdddf1f58579a29a6bf3f00cd8290cb618ab
crimun <crimun@gmail.com>
2024-08-14 15:30:32 -0400

    Sintaxis, Indentación y Comentarios en Python

---
+++ sintaxisIndentacionComentarios.py
@@ -0,0 +1,14 @@
+# Esto es un coentario en Python
+"""
+Este es
+un comentario
+Multilinea
+"""
+# Mostrar por pantalla comando print
+print("Hola Mundo")
+
+# La indentación en Python es estricta
+if 5 > 3:
+    if 5 > 4:
+        print("5 es mayor que 4 y que 3")
+
