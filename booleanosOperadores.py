56846ea5ccf02ec369b903610c16be14d3fc21d5
crimun <crimun@gmail.com>
2024-08-19 11:53:56 -0400

    Booleanos en Python

---
+++ booleanosOperadores.py
@@ -0,0 +1,27 @@
+# Booleanos : tipo de dato que puede tomar 2 valortes Verdadero o Falso (True o False), nos ayudan a tomar deciciones
+
+a = True
+b = False
+
+#print(5 > 3)
+
+# Estructuras que devuelven verdadero (las que tienen contenido)
+
+string = bool("Esto es un texto")
+num = bool(2024)
+lista = bool(['Naranja', 'Manzana'])
+dict = bool({'Nombre': 'Cristian'})
+
+#print(string,num,lista,dict)
+
+# Estructuras que devuelven falso (las que no tienen contenido)
+
+string2 = bool("")
+num2 = bool(0)
+lista2 = bool([])
+dict2 = bool({})
+none = bool(None)
+
+print(string2,num2,lista2,dict2,none)
+
+
