2d599919eb9df2b69b5d4bfc659a8574abb71af4
crimun <crimun@gmail.com>
2024-08-20 09:17:18 -0400

    Bucle FOR en Python

---
+++ bucle-for.py
@@ -0,0 +1,33 @@
+# Bucle FOR: estructura que se repite mientras la condición sea verdadera.
+
+# Como iterar estructuras de datos
+
+## Iterando listas
+
+tecnologias = ['Python','JavaScript','Java','C#','Angular','React','NodeJS','Ruby','R','Django']
+
+# for tecnologia in tecnologias:
+#     print(tecnologia)
+
+# x = 1
+# for tecnologia in tecnologias:
+#     print(f"{x}. {tecnologia}")
+#     x += 1
+
+
+## Iterando un string
+
+# texto = "Hola Mundo"
+
+# for letra in texto:
+#     print(letra)
+
+
+## Iterando un rango
+
+#for x in range(5): # Numeros de 0 al 4, el 5 no se incluye
+#    print(x)
+
+# for x in range(2,11,2): # Numeros pares desde el 2 al 10 (el 11 no se incluye)
+#     print(x)
+
