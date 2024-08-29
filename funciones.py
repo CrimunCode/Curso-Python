b26783b81b54144340d23c6d7900bb5117bf6991
crimun <crimun@gmail.com>
2024-08-29 17:54:35 -0400

    Se añadió una función de prueba para comprender lor Argumentos y los Parámetros

--- funciones.py
+++ funciones.py
@@ -1,3 +1,19 @@
 # Función: es un bloque de código autónomo que se ejecuta cuando lo llamamos, nos va a servir para atomizar y reutilizar código optimizando el mismo
 
-# def funcion()
+# Parámetros: son la lista de variables dentro de los paréntesis
+# Argumentos: son los valores que le enviamos a la función cuando es llamada
+                # Parámetros = variables
+def funcion(profesor, curso, femenino):
+    genero = "el profesor"
+    if femenino:
+        genero = "la profesora"
+    else:
+        genero = "le profesor"
+    print(f"El curso de {curso} lo dará {genero} {profesor}") # Usamos Template String
+
+            # Argumentos = valores
+funcion("Cristian", "Python desde Cero", False)
+funcion("Pedrito", "Cocina", False)
+funcion("Martina", "Manejo", True)
+funcion("Mercedes", "Guitarra", True)
+funcion("Lucho", "Aprendiendo a volar", None)
