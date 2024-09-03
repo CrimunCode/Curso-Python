1706061faa25fc19d4ec4a80ba8bdcc675670c3f
crimun <crimun@gmail.com>
2024-09-03 09:49:56 -0400

    Mejoras en el menú del juego

--- practica_01/juego-adivinanza-numeros.py
+++ practica_01/juego-adivinanza-numeros.py
@@ -6,9 +6,9 @@ def juego_adivinanza():
     adivinado = False
 
     # Primeras lineas del juego, donde se da la bienvenida
-    print("¡Bienvenido al juego de adivinanza de un número!")
+    print("\n¡Bienvenido al juego de adivinanza de un número!")
     print("Debes adivinar un número entre el 1 y el 100")
-    print("¡Intenta adivinarlo!")
+    print("¡Intenta adivinarlo!\n")
     
     while not adivinado:
         # Solicitar un número del 1 al 100
@@ -20,12 +20,35 @@ def juego_adivinanza():
             intentos += 1
 
             if numero < numero_secreto:
-                print(f"El número secreto es mayor a {numero}")
+                print(f"El número secreto es mayor a {numero} \n")
             elif numero > numero_secreto:
-                print(f"El número secreto es menor a {numero}")
+                print(f"El número secreto es menor a {numero}\n")
             else:
+                print("\n")
+                print("*************************************************")
                 print(f"¡Felicitaciones has ganado! \n El número {numero} es el secreto \n Lo has logrado en {intentos} intentos")
+                print("*************************************************")
+                print("\n")
+
+                break
         else:
             error = str(numero)
-            print(f"Por favor introduzca un número válido entre 1 y 100 ({error}, no es un número válido)")
-juego_adivinanza()
+            print(f"Por favor introduzca un número válido entre 1 y 100 (ERROR :({error}), NO es un número válido)")
+while True:
+    print("Bienvenido al menú principal")
+    print("Desea jugar al Juego de Adivinar un números")
+    
+    respuesta = input("Para confirmar presione (s/n)").strip().lower()
+    if respuesta == "s":
+        juego_adivinanza()
+    elif respuesta == "n":
+        print("\n")
+        print("*************************************************")
+        print("Hata pronto")
+        print("*************************************************")
+        break
+    else:
+        error = str(respuesta)
+        print(f"ERROR :({error}), NO es una opción válida")
+        continue
+
