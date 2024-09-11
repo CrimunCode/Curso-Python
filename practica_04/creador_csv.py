473ed94536da82f8c5b0233c02d309f1084f2baf
crimun <crimun@gmail.com>
2024-09-11 14:31:37 -0300

    Archivo para crear un archivo CSV

---
+++ practica_04/creador_csv.py
@@ -0,0 +1,16 @@
+from operator import index
+import pandas as pd
+
+# Dataframe : es la información básica con el nombre d elas piezas y centímetros para poder armar el Excel.
+
+data = {
+    "Piezas:": ["Pata", "Tablero", "Puerta", "Estante", "Panel Lateral"],
+    "Centimetros:": [40, 120, 60, 30, 180],
+}
+
+# Creamos en Dataframe
+df = pd.DataFrame(data)
+
+# Guardar el Dataframe en un archivo Excel
+
+df.to_csv("muebles_medidas.csv", index=False)
