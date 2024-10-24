d4692780dfd1830a3b86307fd26a6f38f395191e
crimun <crimun@gmail.com>
2024-10-24 08:57:43 -0300

    Se añadió más contenido sobre las tuplas

---
+++ tuplas_2.py
@@ -0,0 +1,14 @@
+# Tuplas: las tuplas no son mutables o modificables son constantes
+
+# Declarar una tupla
+colores = ('Rojo', 'Azul', 'Verde', 'Amarillo')
+print('\n', colores ,'\n')
+
+# Acceder a un solo elemento de la tupla, indicando el indice
+print(colores[1] ,'\n')
+
+# Contar elementos de una tupla o lista
+print(len(colores),'\n')
+colores_lista = ['Rojo', 'Azul', 'Verde', 'Amarillo', 'Naranja']
+print(len(colores_lista), '\n')
+
