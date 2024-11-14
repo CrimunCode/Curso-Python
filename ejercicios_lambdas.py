28fb739810dce6898e641064905e6e45d8dc2bb7
crimun <crimun@gmail.com>
2024-11-14 08:24:28 -0300

    Más ejercicios con la funcion Lambda

--- ejercicios_lambdas.py
+++ ejercicios_lambdas.py
@@ -1,7 +1,7 @@
 #  Calcular el área de un circulo (Pi * radio * radio)
 
 # variables
-radio = float(input("\nIngrese el radio del circulo: "))
+# radio = float(input("\nIngrese el radio del circulo: "))
 
 # Constantes
 PI = 3.14159265359
@@ -10,7 +10,16 @@ PI = 3.14159265359
 calcular_area = lambda radio : PI * radio * radio
 
 # Llamada a la función
-area = round(calcular_area(radio), 2)
+# area = round(calcular_area(radio), 2)
 
 # Mostrar resultado
-print(area)
+# print(area)
+
+# Utilizar la sintaxis de declaración y llamada en una linea para mostrar un saludo en la consola con el nombre en un print()
+
+(lambda nombre: print(f"\nQue tal {nombre}!")) ("CrimunCode")
+
+# Mostra esta frase en la consola: El color se encuentra en la posición 1 de la lista con una funcion lambda
+colores = ["Rojo","Azul","Amarillo","Verde"]
+
+(lambda color: print(f"\nEl color {color} se encuentra en la posición {colores.index(color)} de la lista."))(color = input("Ingrese un color para buscar su indice: "))
