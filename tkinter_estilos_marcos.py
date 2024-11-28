9fbae36e8b0759ff0cce4b46eebd07aebfdaa600
crimun <crimun@gmail.com>
2024-11-28 09:37:23 -0300

    Estilos y Marcos en Tkinter

---
+++ tkinter_estilos_marcos.py
@@ -0,0 +1,68 @@
+# Iniciación a los estilos y marcos de Tkinter
+
+# Imortaciones
+import tkinter as tk
+
+# Ventana principal
+root = tk.Tk()
+# Titulo de la ventana
+root.title("Estilos y marcos de Tkinter")
+# Tamaño y ubicación ventana
+# root.geometry("320x240+2200+400")
+
+# Marco o frame
+marco1 = tk.LabelFrame(root, 
+                      text="entrada de datos",
+                      padx=20,
+                      pady=20,
+                      )
+marco1.pack(padx=15, pady=15) # Padding para el marco con respecto a la ventana
+
+marco2 = tk.LabelFrame(root,
+                      text="Enviar",
+                      padx=20,
+                      pady=20,
+                      )
+marco2.pack(padx=5, pady=15) # Padding para el marco con respecto a la ventana
+
+marco3 = tk.LabelFrame(root,
+                      text="Resultado",
+                      padx=20,
+                      pady=20,
+                      )
+marco3.pack(padx=5, pady=15) # Padding para el marco con respecto a la ventana
+
+# Entrada de datos
+entrada = tk.Entry(marco1, 
+                   background="chocolate1", # Color de fondo
+                   border=3, # Borde
+                   foreground="Blue3", # color de la fuente
+                   width=30, # Ancho en tamaño de carácteres
+                   )
+entrada.pack()
+entrada.insert(0, "Ecriba su nombre...")
+entrada.bind("<Button-1>", lambda e: entrada.delete(0, tk.END))
+
+# Funciones
+def f_enviar():
+    nombre = entrada.get()
+    tk.Label(marco3, 
+             text=f"Hola {nombre}",
+             background="skyblue",
+             width=30, # Ancho en pixeles
+             ).pack()
+    entrada.delete(0, tk.END)
+    entrada.insert(0, "Escriba su nombre...")
+
+# Boton enviar
+btn = tk.Button(marco2, 
+                text="Enviar", 
+                command=f_enviar,
+                background="deepskyblue",
+                foreground="gray98",
+                border=3,
+                width=30, # Ancho en pixeles
+                ).pack()
+
+# Bucle de ejecución
+root.mainloop()
