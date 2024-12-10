096f658900ba9f98fb0ebbc019ae9e32b11aec61
crimun <crimun@gmail.com>
2024-12-10 12:09:02 -0300

    Como importar sub módulos de tkinter en Python

---
+++ ejercicios_modulos2.py
@@ -0,0 +1,5 @@
+# Ejercicio 01: Usar un método llamado showinfo() del múdulo messagebox de tkinter
+# Importaciones
+import tkinter.messagebox as tkmb
+
+tkmb.showinfo("www.programacionfacil.org", "Este es un mensaje informativo")
